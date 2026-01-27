20d90926040d8db0f8897e0a86408452021d6622
Maksim Vabishchevich <oldvabik@gmail.com>
2026-01-27 11:27:49 +0300

    add actuator

--- src/main/java/org/oldvabik/paymentservice/config/SecurityConfig.java
+++ src/main/java/org/oldvabik/paymentservice/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/payments/**").authenticated()
+                        .requestMatchers("/actuator/health", "/actuator/health/**").permitAll()
                         .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
